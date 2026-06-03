83c54476c9d4d412ef8397c94e9d9d45bfe15d1f
ac150897@avcol.school.nz <ac150897@avcol.school.nz>
2026-06-03 12:30:54 +1200

    added a user model

---
+++ SchoolEquipmentHire/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace SchoolEquipmentHire.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public int YearLevel { get; set; }
+
+        public RoleType Role { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+
+    public enum RoleType
+    {
+        Student,
+        Admin
+    }
+}
