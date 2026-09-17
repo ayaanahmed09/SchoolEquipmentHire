02860e075da0a6b99c2fc1d2b15c75a318db00d6
ayaanahmed09 <ac150897@avcol.school.nz>
2026-09-18 10:05:12 +1200

    register works, need to update Ui for homepage, booking, check equipment index page at the end to fix images problem

--- SchoolEquipmentHire/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SchoolEquipmentHire/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -131,8 +131,12 @@ public class RegisterModel : PageModel
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+            user.FirstName = Input.FirstName;
+            user.LastName = Input.LastName;
+            user.YearLevel = Input.YearLevel;
+            user.Role = Enum.Parse<RoleType>(Input.Role);
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+            await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
